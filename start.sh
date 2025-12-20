1b74fc54045cd4404cebd3d697b597f3d9d2c584
Aditya Hire <adityahire08@gmail.com>
2025-12-20 15:17:06 +0530

    Add set -e and debug echo to start.sh for Railway migration troubleshooting

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+echo "=== Running start.sh: applying migrations and starting Gunicorn ==="
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 exec gunicorn medicine_project.wsgi:application --bind 0.0.0.0:${PORT:-8000}
